99361bee0d9d84514a8780ff8f9b02cbc901e5ac
egg <1156956096@qq.comm>
2019-12-19 13:54:06 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入axios
+import axios from 'axios'
 // 引入初始化样式
 import './styles/index.less'
 // 引入element
@@ -9,7 +11,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 在axios统一导入之前 设置一个常态值
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 赋值全局对象
+Vue.prototype.$axios = axios
+// 相当于所有的Vue的实例就都有了这个$axios属性
 new Vue({
   router,
   render: h => h(App)
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>主页</div>
+  <el-container>
+    <el-aside>导航</el-aside>
+    <el-container>
+      <el-header>头部</el-header>
+      <el-main>内容</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+
 }
 </script>
 
-<style>
+<style scoped lang="less">
 
 </style>
