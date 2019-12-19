02b61f15a283fa0c13d2e3abc533fc52e7adbea0
egg <1156956096@qq.comm>
2019-12-19 17:52:59 +0800

    头部

--- src/components/home/home-header.vue
+++ src/components/home/home-header.vue
@@ -1,13 +1,36 @@
 <template>
-  <div></div>
+  <el-row type="flex" justify="space-between">
+    <el-col :span="6">
+      <i class="el-icon-s-unfold"></i>
+      <span style="font-size:16px">江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <el-col :span="6">
+      <el-row type="flex" justify="end" align="middle">
+        <img src="../../assets/img/avatar.jpg" alt=""/>
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            路飞
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>git地址</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang="less" scoped>
+img{
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+}
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,19 +4,24 @@
       <home-aside></home-aside>
     </el-aside>
     <el-container>
-      <el-header>头部</el-header>
+      <el-header>
+        <home-header></home-header>
+      </el-header>
       <el-main>内容</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-// 引入
+// 引入左导航
 import homeaside from '../../components/home/home-aside'
+// 引入头部
+import homeheader from '../../components/home/home-header'
 export default {
   // 注册组件
   components: {
-    'home-aside': homeaside
+    'home-aside': homeaside,
+    'home-header': homeheader
   }
 }
 </script>
