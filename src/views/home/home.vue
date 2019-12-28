2dce73cc27aa1f9b2f1e275248b3be65a4492f4a
egg <1156956096@qq.comm>
2019-12-28 16:40:37 +0800

    引入echarts

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,17 +1,44 @@
 <template>
 <!-- 二级组件 -->
-<div class="pic"></div>
+<div class="pic">
+    <div class="echart" ref="myecharts"></div>
+</div>
 </template>
 
 <script>
+import Echart from 'echarts'
 export default {
+  mounted () {
+    //   获取实例
+    let myecharts = Echart.init(this.$refs.myecharts)
+    //
+    myecharts.setOption(
+      {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line'
+        }]
+      }
+    )
+  }
 
 }
 </script>
 
 <style lang="less" scoped>
 .pic {
-    background-image: url('../../assets/img/timg (1).png');
-    height: 100vh;
+    // background-image: url('../../assets/img/timg (1).png');
+    // height: 100vh;
+    .echart{
+        width: 600px;
+        height: 400px;
+    }
 }
 </style>
